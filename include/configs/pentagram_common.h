2f7b9af2569c6c6f31f585753b545e5334df178e
denzel.lin <denzel.lin@sunplus.com>
2018-05-10 16:04:25 +0800

    [uboot] commit generic boot flow for zebu [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] 	Add boot flow for running zebu.
    	On zebu, we still use sp_go which do not calculate
    	checksum of kernel image, and it can decrease bootup
    	time.
    
    [Test Case or Method] N/A
    [Compile] N/A [Coverity] N/A [Selftest] N/A
    [GPL-licensed or Third-party Source] N/A

--- include/configs/pentagram_common.h
+++ include/configs/pentagram_common.h
@@ -82,19 +82,48 @@
 #include <asm/arch/sp_bootmode_bitmap_sc7xxx.h>
 #endif
 
-#if defined(CONFIG_SYS_ENV_8388)
-
-#if 0 /* if open this, need to mark VMLINUX in ipack/update_all.sh */
-
+/*
+ * In the beginning, bootcmd will check bootmode in SRAM and the flag
+ * if_zebu to choose different boot flow :
+ * romter_boot
+ *      kernel is in SPI nor offset 6M, and dtb is in offset 128K.
+ *      kernel will be loaded to 0x307FC0 and dtb will be loaded to 0x2FFFC0.
+ *      Then bootm 0x307FC0 - 0x2FFFC0.
+ * zmem_boot 
+ *      kernel is preloaded to 0x307FC0 and dtb is preloaded to 0x2FFFC0.
+ *      Then sp_go 0x308000 0x300000.
+ *      sp_go do not handle header so use 0x300080 to skip header and dtb have
+ *      additional header so real dtb is on 0x300000.
+ * emmc_boot
+ *      kernel is stored in emmc LBA CONFIG_SRCADDR_KERNEL and dtb is
+ *      stored in emmc LBA CONFIG_SRCADDR_DTB. 
+ *      kernel will be loaded to 0x307FC0 and dtb will be loaded to 0x2FFFC0.
+ *      Then bootm 0x307FC0 - 0x2FFFC0. 
+ * zebu_emmc_boot
+ *      kernel is stored in emmc LBA CONFIG_SRCADDR_KERNEL and dtb is
+ *      stored in emmc LBA CONFIG_SRCADDR_DTB.
+ *	kernel will be loaded to 0x307FC0 and dtb will be loaded to 0x2FFFC0.
+ *	Then sp_go 0x308000 0x300000.
+ *	sp_go do not handle header so use 0x300080 to skip header and dtb have
+ *      additional header so real dtb is on 0x300000.
+ */
 #define CONFIG_BOOTCOMMAND \
 "echo [scr] bootcmd started; " \
 "md.l ${bootinfo_base} 1; " \
 "if itest.l *${bootinfo_base} == " __stringify(SPI_NOR_BOOT) "; then " \
 	"echo [scr] romter boot; " \
-	"run romter_boot; " \
+	"if itest ${if_zebu} == 1; then " \
+		"run zmem_boot; " \
+	"else " \
+		"run romter_boot; " \
+	"fi; " \
 "elif itest.l *${bootinfo_base} == " __stringify(EMMC_BOOT) "; then " \
 	"echo [scr] emmc boot; " \
-	"run emmc_boot; " \
+	"if itest ${if_zebu} == 1; then " \
+		"run xxzebu_emmc_boot; " \
+	"else " \
+		"run emmc_boot; " \
+	"fi; " \
 "elif itest.l *${bootinfo_base} == " __stringify(NAND_BOOT) "; then " \
 	"echo [scr] nand boot; " \
 	"run nand_boot; " \
@@ -111,6 +140,7 @@
 "addr_dst_kernel="		__stringify(DSTADDR_KERNEL) "\0" \
 "addr_dst_dtb="			__stringify(DSTADDR_DTB) "\0" \
 "addr_tmp_header="		__stringify(TMPADDR_HEADER) "\0" \
+"if_zebu="			__stringify(CONFIG_SYS_ENV_ZEBU) "\0" \
 "be2le=md ${tmpaddr} 1; " \
 	"setexpr byte *${tmpaddr} '&' 0x000000ff; " \
 	"setexpr tmpval $tmpval + $byte; " \
@@ -145,29 +175,43 @@
 	"setexpr sz_kernel ${tmpval} + 0x40; " \
 	"setexpr sz_kernel ${sz_kernel} + 0x200; setexpr sz_kernel ${sz_kernel} / 0x200; " \
 	"mmc read ${addr_dst_kernel} ${addr_src_kernel} ${sz_kernel}; " \
-	"bootm ${addr_dst_kernel} - ${addr_dst_dtb}\0"
-#else
+	"bootm ${addr_dst_kernel} - ${addr_dst_dtb}\0" \
+"zmem_boot=setexpr addr_dst_kernel ${addr_dst_kernel} + 0x40; " \
+	"setexpr addr_dst_dtb ${addr_dst_dtb} + 0x40; " \
+	"sp_go ${addr_dst_kernel} ${addr_dst_dtb}\0" \
+"zebu_emmc_boot=mmc rescan; mmc part; " \
+	"mmc read ${addr_tmp_header} ${addr_src_dtb} 0x1; " \
+	"setenv tmpval 0; setexpr tmpaddr ${addr_tmp_header} + 0x4; run be2le; " \
+	"setexpr sz_dtb ${tmpval} + 0x28; " \
+	"setexpr sz_dtb ${sz_dtb} + 0x200; setexpr sz_dtb ${sz_dtb} / 0x200; " \
+	"mmc read ${addr_dst_dtb} ${addr_src_dtb} ${sz_dtb}; " \
+	"mmc read ${addr_tmp_header} ${addr_src_kernel} 0x1; " \
+	"setenv tmpval 0; setexpre tmpaddr ${addr_tmp_header} + 0x0c; run be2le; " \
+	"setexpr sz_kernel ${tmpval} + 0x40; " \
+	"setexpr sz_kernel ${sz_kernel} + 0x200; setexpr sz_kernel ${sz_kernel} / 0x200; " \
+	"mmc read ${addr_dst_kernel} ${addr_src_kernel} ${sz_kernel}; " \
+	"setexpr addr_dst_kernel ${addr_dst_kernel} + 0x40; " \
+	"setexpr addr_dst_dtb ${addr_dst_dtb} + 0x40; " \
+	"sp_go ${addr_dst_kernel} ${addr_dst_dtb}\0"
+#if 0
+/* romter test booting command */
 #define CONFIG_BOOTCOMMAND      "echo bootcmd started ; sp_preboot dump ; sp_preboot ; printenv ; \
 echo [cmd] cp.l 0x98600000 0x307FC0 0x280000 ; \
 cp.l 0x98600000 0x307FC0 0x280000 ; \
 echo [cmd] cp.l 0x98020000 0x2FFFC0 0x400 ; \
 cp.l 0x98020000 0x2FFFC0 0x400 ; \
 sp_go 0x308000 0x300000"
-#endif /* if 0 */
 
-#elif defined(CONFIG_SYS_ENV_ZEBU)
-#if defined (CONFIG_SD_BOOT)
+/* zebu emmc booting test command */
 #define CONFIG_BOOTCOMMAND      "echo [scr] emmc bootcmd started ; \
 mmc rescan ; mmc part ; \
 mmc read 0x2fffc0 0x1422 0x1 ; md 0x2fffc0 0x60 ; \
 mmc read 0x307fc0 0x1822 0x1 ; md 0x307fc0 0x60 ; \
 mmc read 0x2fffc0 0x1422 0xa ; mmc read 0x307fc0 0x1822 0x30f0 ; sp_go 0x308000 0x300000"
-#elif defined (CONFIG_NAND_BOOT)
-#else /* zmem */
+
+/* zebu zmem booting test command */
 #define CONFIG_BOOTCOMMAND      "echo [scr] zmem bootcmd started ; sp_go 0x308000 0x300040"
 #endif
-
-#endif /* CONFIG_SYS_ENV_ */
 
 /* MMC related configs */
 #define CONFIG_SUPPORT_EMMC_BOOT 
