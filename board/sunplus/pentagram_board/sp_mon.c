03e55512561b80ae912125e2741cd83dfe86d868
denzel.lin <denzel.lin@sunplus.com>
2018-06-06 17:30:49 +0800

    [uboot] Let lreg/wreg support a/b chip [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] 	Added parameter for select a/b chip.
    	Usage example :
    	lreg a 1
    	(show a chip group 1)
    
    	wreg b 1 1 0x1234
    	(write 0x1234 to b chip group 1.1)
    
    [Test Case or Method] N/A
    [Compile] N/A [Coverity] N/A [Selftest] N/A
    [GPL-licensed or Third-party Source] N/A

--- board/sunplus/pentagram_board/sp_mon.c
+++ board/sunplus/pentagram_board/sp_mon.c
@@ -8,7 +8,8 @@
 #include <command.h>
 #include <linux/compiler.h>
 
-#define RGST_OFFSET	0x9C000000
+#define RGST_OFFSET_A	0x9EC00000
+#define RGST_OFFSET_B   0x9C000000
 
 #define ENTER		0x0D
 #define SPACE		0x20
@@ -30,12 +31,12 @@
 #define dbg_printf	printf
 
 /* Print specific group of registers */
-void prn_regs(unsigned int regGroupNum)
+void prn_regs(unsigned int regGroupNum, unsigned int rgst_offset)
 {
 	unsigned int i = 0;
 	unsigned int regBaseAddr = 0;
 
-	regBaseAddr = RGST_OFFSET + regGroupNum * 32 * 4; /* unit: bytes */
+	regBaseAddr = rgst_offset + regGroupNum * 32 * 4; /* unit: bytes */
 	for(i = 0; i < 32; i++)
 		dbg_printf("Read G%u.%2u = 0x%08x (%u)\n", regGroupNum, i,
 			   *((volatile unsigned int *)(regBaseAddr)+i),
@@ -44,11 +45,12 @@ void prn_regs(unsigned int regGroupNum)
 }
 
 /* Write 4 bytes to a specific register in a certain group */
-void write_regs(unsigned int regGroupNum, unsigned int regIndex, unsigned int value)
+void write_regs(unsigned int regGroupNum, unsigned int regIndex,
+		unsigned int value, unsigned int rgst_offset)
 {
 	unsigned int targetAddr = 0;
 
-	targetAddr = RGST_OFFSET + (regGroupNum * 32 * 4) + regIndex * 4; /* unit: bytes */
+	targetAddr = rgst_offset + (regGroupNum * 32 * 4) + regIndex * 4; /* unit: bytes */
 	*((volatile unsigned int *) targetAddr) = value;
 
 	dbg_printf("Write G%u.%2u = 0x%08x (%u)\n", regGroupNum, regIndex, value, value);
@@ -63,12 +65,22 @@ void write_regs(unsigned int regGroupNum, unsigned int regIndex, unsigned int va
 static int do_reg_lreg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	int	regGroupNum = 0;
+	unsigned int rgst_offset = 0;
 
-	if (argc == 3) {
+	if (argc == 4) {
 
-	} else if (argc == 2){
-		regGroupNum = simple_strtoul(argv[1], NULL, 0);
-		prn_regs(regGroupNum);
+	} else if (argc == 3) {
+		if (strncmp(argv[1], "a", 1) == 0)
+			rgst_offset = RGST_OFFSET_A;
+		else if (strncmp(argv[1], "b", 1) == 0)
+			rgst_offset = RGST_OFFSET_B;
+		else {
+			printf("Invalid chip. It should be \'a\' or \'b\'.\n");
+			return CMD_RET_USAGE;
+		}
+
+		regGroupNum = simple_strtoul(argv[2], NULL, 0);
+		prn_regs(regGroupNum, rgst_offset);
 	} else
 		return CMD_RET_USAGE;
 
@@ -82,14 +94,24 @@ static int do_reg_lreg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
  */
 static int do_reg_wreg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
-	if (argc == 4){
+	if (argc == 5) {
 		unsigned int regGroupNum = 0, offset = 0, value = 0;
+		unsigned int rgst_offset = -1;
 
-		regGroupNum = simple_strtoul(argv[1], NULL, 0);
-		offset = simple_strtoul(argv[2], NULL, 0);
-		value = simple_strtoul(argv[3], NULL, 0);
+		if (strncmp(argv[1], "a", 1) == 0)
+			rgst_offset = RGST_OFFSET_A;
+		else if (strncmp(argv[1], "b", 1) == 0)
+			rgst_offset = RGST_OFFSET_B;
+		else {
+			printf("Invalid chip. It should be be \'a\' or \'b\'.\n");
+			return CMD_RET_USAGE;
+		}
 
-		write_regs(regGroupNum, offset, value);
+		regGroupNum = simple_strtoul(argv[2], NULL, 0);
+		offset = simple_strtoul(argv[3], NULL, 0);
+		value = simple_strtoul(argv[4], NULL, 0);
+
+		write_regs(regGroupNum, offset, value, rgst_offset);
 	} else
 		return CMD_RET_USAGE;
 	return 0;
@@ -97,13 +119,13 @@ static int do_reg_wreg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 
 /**************************************************/
 U_BOOT_CMD(
-	lreg,	2,	1,	do_reg_lreg,
+	lreg,	3,	1,	do_reg_lreg,
 	"Sunplus MON : load register (by group)",
-	"[register group #]"
+	"[a|b] [register group #]"
 );
 
 U_BOOT_CMD(
-	wreg,	4,	1,	do_reg_wreg,
+	wreg,	5,	1,	do_reg_wreg,
 	"Sunplus MON : write 4 bytes to register",
-	"[register group #] [register # in group] [value (4 bytes)]"
+	"[a|b] [register group #] [register # in group] [value (4 bytes)]"
 );
