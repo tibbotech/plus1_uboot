52a1eed738cd5e4fb011244aceab52d04dda3d35
vincent.shih <vincent.shih@sunplus.com>
2019-02-18 16:30:21 +0800

    [OTP] Modify the OTP read function [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] As title [Test   Case] N/A [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

--- drivers/soc/sunplus/sp_otp.c
+++ drivers/soc/sunplus/sp_otp.c
@@ -1,52 +1,66 @@
 #include <common.h>
 #include <asm/io.h>
 
-#define OTP_WAIT_MICRO_SECONDS    100
+/*
+ * QAC628 OTP memory contains 8 banks with 4 32-bit words. Bank 0 starts
+ * at offset 0 from the base.
+ *
+ */
+#define QAC628_OTP_NUM_BANKS		   8
+#define QAC628_OTP_WORDS_PER_BANK	   4
+#define QAC628_OTP_WORD_SIZE		   sizeof(u32)
+#define QAC628_OTP_SIZE		           (QAC628_OTP_NUM_BANKS * \
+					            QAC628_OTP_WORDS_PER_BANK * \
+					            QAC628_OTP_WORD_SIZE)
+#define QAC628_OTP_BIT_ADDR_OF_BANK    (8 * QAC628_OTP_WORD_SIZE * \
+	                            QAC628_OTP_WORDS_PER_BANK)
+	                            
+#define OTP_READ_TIMEOUT               20000
 
-#define OTPRX_2_BASE_ADR          0x9C002800
-#define OTPRX_BASE_ADR            0x9C00AF80
-#define HB_GPIO                   0x9C00AF00
+#define OTPRX_2_BASE_ADR               0x9C002800
+#define OTPRX_BASE_ADR                 0x9C00AF80
+#define HB_GPIO                        0x9C00AF00
 
 /* OTP register map */
-#define OTP_PROGRAM_CONTROL       0x0C
-#define PIO_MODE                  0x07
+#define OTP_PROGRAM_CONTROL            0x0C
+#define PIO_MODE                       0x07
 
-#define OTP_PROGRAM_ADDRESS       0x10
-#define PROG_OTP_ADDR             0x1FFF
+#define OTP_PROGRAM_ADDRESS            0x10
+#define PROG_OTP_ADDR                  0x1FFF
 
-#define OTP_PROGRAM_PGENB         0x20
-#define PIO_PGENB                 0x01
+#define OTP_PROGRAM_PGENB              0x20
+#define PIO_PGENB                      0x01
 
-#define OTP_PROGRAM_ENABLE        0x24
-#define PIO_WR                    0x01
+#define OTP_PROGRAM_ENABLE             0x24
+#define PIO_WR                         0x01
 
-#define OTP_PROGRAM_VDD2P5        0x28
-#define PROGRAM_OTP_DATA          0xFF00
-#define PROGRAM_OTP_DATA_SHIFT    8
-#define REG25_PD_MODE_SEL         0x10
-#define REG25_POWER_SOURCE_SEL    0x02
-#define OTP2REG_PD_N_P            0x01
+#define OTP_PROGRAM_VDD2P5             0x28
+#define PROGRAM_OTP_DATA               0xFF00
+#define PROGRAM_OTP_DATA_SHIFT         8
+#define REG25_PD_MODE_SEL              0x10
+#define REG25_POWER_SOURCE_SEL         0x02
+#define OTP2REG_PD_N_P                 0x01
 
-#define OTP_PROGRAM_STATE         0x2C
-#define OTPRSV_CMD3               0xE0
-#define OTPRSV_CMD3_SHIFT         5
-#define TSMC_OTP_STATE            0x1F
+#define OTP_PROGRAM_STATE              0x2C
+#define OTPRSV_CMD3                    0xE0
+#define OTPRSV_CMD3_SHIFT              5
+#define TSMC_OTP_STATE                 0x1F
 
-#define OTP_CONTROL               0x44
-#define PROG_OTP_PERIOD           0xFFE0
-#define PROG_OTP_PERIOD_SHIFT     5
-#define OTP_EN_R                  0x01
+#define OTP_CONTROL                    0x44
+#define PROG_OTP_PERIOD                0xFFE0
+#define PROG_OTP_PERIOD_SHIFT          5
+#define OTP_EN_R                       0x01
 
-#define OTP_CONTROL2              0x48
-#define OTP_RD_PERIOD             0xFF00
-#define OTP_RD_PERIOD_SHIFT       8
-#define OTP_READ                  0x04
+#define OTP_CONTROL2                   0x48
+#define OTP_RD_PERIOD                  0xFF00
+#define OTP_RD_PERIOD_SHIFT            8
+#define OTP_READ                       0x04
 
-#define OTP_STATUS                0x4C
-#define OTP_READ_DONE             0x10
+#define OTP_STATUS                     0x4C
+#define OTP_READ_DONE                  0x10
 
-#define OTP_READ_ADDRESS          0x50
-#define RD_OTP_ADDRESS            0x1F
+#define OTP_READ_ADDRESS               0x50
+#define RD_OTP_ADDRESS                 0x1F
 
 struct otprx_sunplus {
 	u32 sw_trim;
@@ -92,27 +106,31 @@ int readOTPData(int addr, char *value)
 {        	
 	unsigned int addr_data;
 	unsigned int byte_shift;
-	unsigned int data;
-    u32 timeout = OTP_WAIT_MICRO_SECONDS;
+	unsigned int status;
+    u32 timeout = OTP_READ_TIMEOUT;
 	
-	addr_data = (addr % 16) / 4;
-	byte_shift = ((addr % 16) % 4);
+	addr_data = addr % (QAC628_OTP_WORD_SIZE * QAC628_OTP_WORDS_PER_BANK);
+	addr_data = addr_data / QAC628_OTP_WORD_SIZE;
+	
+	byte_shift = addr % (QAC628_OTP_WORD_SIZE * QAC628_OTP_WORDS_PER_BANK);
+	byte_shift = byte_shift % QAC628_OTP_WORD_SIZE;
 		
 	writel(0x0, &regs->otp_cmd_status);
 
-	addr = (addr / 16) * 4 * 32;
+	addr = addr / (QAC628_OTP_WORD_SIZE * QAC628_OTP_WORDS_PER_BANK);
+	addr = addr * QAC628_OTP_BIT_ADDR_OF_BANK;
 	writel(addr, &regs->otp_addr);
 	
 	writel(0x1E04, &regs->otp_cmd);
 	
 	do
     {
-        udelay(1000);
+        udelay(10);
 		if (timeout-- == 0)
 		    return -1;
 
-        data = readl(&regs->otp_cmd_status);
-    } while((data & OTP_READ_DONE) != OTP_READ_DONE);
+        status = readl(&regs->otp_cmd_status);
+    } while((status & OTP_READ_DONE) != OTP_READ_DONE);
 
 	*value = (otp_data->hb_gpio_rgst_bus32[8+addr_data] >> (8 * byte_shift)) & 0xFF;
 
@@ -130,7 +148,7 @@ static int do_read_otp(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 	{
 	    addr = simple_strtoul(argv[1], NULL, 0);
 
-		if (addr >= 128)
+		if (addr >= QAC628_OTP_SIZE)
 			return CMD_RET_USAGE;
 		
 	    if (readOTPData(addr, &value) == -1)
@@ -146,7 +164,7 @@ static int do_read_otp(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 	    printf(" (byte No.)   (data)\n");
 	    j = 0;
 		
-		for (addr = 0 ; addr < 127; addr += 16)
+		for (addr = 0 ; addr < (QAC628_OTP_SIZE - 1); addr += (QAC628_OTP_WORD_SIZE * QAC628_OTP_WORDS_PER_BANK))
 		{
 	        if (readOTPData(addr, &value) == -1)
 			    return CMD_RET_FAILURE;
