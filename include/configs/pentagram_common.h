06d0b7ceeb471e375a8207bde33cc800c2510dc2
haiq.tang <haiq.tang@sunmedia.com.cn>
2019-09-09 00:38:49 -0700

    [Uboot][NAND boot]spinand used ubifs to replace the ramfs     [Mantis   ID] N/A     [Bug    Desc] SP7021-532     [Modify Desc]     [Bug    Desc] SP7021-532     [Modify Desc]                   1.support ubi cmd in uboot                   2.modify the bootargs,transfer the nand partition info to kernel     [Test   Case] N/A     [Compile] OK     [Coverity] N/A     [Selftest] N/A     [GPL-licensed & Third-party Source] N/A

--- include/configs/pentagram_common.h
+++ include/configs/pentagram_common.h
@@ -108,6 +108,7 @@
 #endif
 
 #ifdef CONFIG_SP_SPINAND
+#define CONFIG_MTD_PARTITIONS
 #define CONFIG_SYS_MAX_NAND_DEVICE   1
 #define CONFIG_SYS_NAND_SELF_INIT
 #define CONFIG_SYS_NAND_BASE    0x9c002b80
@@ -287,7 +288,7 @@
 	"setexpr sz_kernel ${sz_kernel} + 72; " \
 	"setexpr sz_kernel ${sz_kernel} + 0x200; setexpr sz_kernel ${sz_kernel} / 0x200; " \
 	"mmc read ${addr_dst_kernel} ${addr_src_kernel} ${sz_kernel}; " \
-	"setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p7  user_debug=255 rootwait ;" \
+	"setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p7 rw user_debug=255 rootwait ;" \
 	"bootm ${addr_dst_kernel} - ${addr_dst_dtb}\0" \
 "qk_emmc_boot=mmc read ${addr_tmp_header} ${addr_src_dtb} 0x1; " \
 	"setenv tmpval 0; setexpr tmpaddr ${addr_tmp_header} + 0x0c; run be2le; " \
@@ -313,6 +314,7 @@
 	"setexpr sz_kernel ${sz_kernel} + 72; " \
 	dbg_scr("echo from kernel partition to ${addr_dst_kernel} sz ${sz_kernel}; ") \
 	"nand read ${addr_dst_kernel} kernel ${sz_kernel}; " \
+	"setenv bootargs console=ttyS0,115200 earlyprintk root=ubi0:rootfs rw ubi.mtd=8,2048 rootflags=sync rootfstype=ubifs mtdparts=sp_spinand:128k(nand_header),128k(xboot1),768k(uboot1),3m(uboot2),512k(env),512k(env_redund),256k(dtb),15m(kernel),-(rootfs) user_debug=255 rootwait ;" \
 	"bootm ${addr_dst_kernel} - ${addr_dst_dtb}\0" \
 "qk_zmem_boot=sp_go ${addr_dst_kernel} ${addr_dst_dtb}\0" \
 "zmem_boot=bootm ${addr_dst_kernel} - ${addr_dst_dtb}\0" \
