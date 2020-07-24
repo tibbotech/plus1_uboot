f832705bb3f6d360556a450d1389edb514aca23d
hammer.hsieh <hammer.hsieh@sunplus.com>
2020-07-24 09:31:02 +0800

    [drivers][video][sunplus][i143][mipi] add hdmitx setting for mipi.

--- drivers/video/sunplus/i143/mipi/sp_videoin.c
+++ drivers/video/sunplus/i143/mipi/sp_videoin.c
@@ -555,21 +555,25 @@ static void hdmi_clk_init(int mode)
 		MOON5_REG->cfg[6]		= 0xFFFF0020; 	//G5.6
     }
     else if (mode == 1) { //HDMITX 720x576 50Hz as default setting
-        ; //TBD
+        MOON4_REG->plltv_ctl[0] = 0xFFFF0000; 	//G4.15
+        MOON4_REG->plltv_ctl[1] = 0xFFFF0014; 	//G4.16
+        MOON4_REG->plltv_ctl[2] = 0xFFFF2040; 	//G4.17
+		MOON4_REG->plltv_ctl[3] = 0xFFFF0040; 	//G4.18
+		MOON5_REG->cfg[6]		= 0xFFFF0020; 	//G5.6
     }
     else if (mode == 2) { //HDMITX 1280x720 59.94Hz as default setting
-		; //TBD
-        //MOON4_REG->plltv_ctl[0] = 0xFFFF0014; 	//G4.15
-        //MOON4_REG->plltv_ctl[1] = 0xFFFF0000; 	//G4.16
-        //MOON4_REG->plltv_ctl[2] = 0xFFFF2040; 	//G4.17
-		//MOON4_REG->plltv_ctl[3] = 0xFFFF0040; 	//G4.18
+        MOON4_REG->plltv_ctl[0] = 0xFFFF57ff; 	//G4.15
+        MOON4_REG->plltv_ctl[1] = 0xFFFF2050; 	//G4.16
+        MOON4_REG->plltv_ctl[2] = 0xFFFF0145; 	//G4.17
+		MOON4_REG->plltv_ctl[3] = 0xFFFF0004; 	//G4.18
+		MOON5_REG->cfg[6]		= 0xFFFF0020; 	//G5.6
 	}
 	else if (mode == 3) { //HDMITX 1920x1080 59.94Hz as default setting
-		; //TBD
-        //MOON4_REG->plltv_ctl[0] = 0xFFFF0014; 	//G4.15
-        //MOON4_REG->plltv_ctl[1] = 0xFFFF0000; 	//G4.16
-        //MOON4_REG->plltv_ctl[2] = 0xFFFF2040; 	//G4.17
-		//MOON4_REG->plltv_ctl[3] = 0xFFFF0040; 	//G4.18
+        MOON4_REG->plltv_ctl[0] = 0xFFFF47ff; 	//G4.15
+        MOON4_REG->plltv_ctl[1] = 0xFFFF2050; 	//G4.16
+        MOON4_REG->plltv_ctl[2] = 0xFFFF0145; 	//G4.17
+		MOON4_REG->plltv_ctl[3] = 0xFFFF0004; 	//G4.18
+		MOON5_REG->cfg[6]		= 0xFFFF0020; 	//G5.6
 	}
 
 }
@@ -582,6 +586,7 @@ static void hdmitx_set_timming(int mode)
         G380_HDMITX_REG->g380_reserved[8] = 0x00001001;
 
         G381_HDMITX_REG->g381_reserved[16] = 0x00000140;
+		G381_HDMITX_REG->g381_reserved[20] = 0x00000716;
 
         G382_HDMITX_REG->g382_reserved[12] = 0x00000043;
         G382_HDMITX_REG->g382_reserved[16] = 0x0000e411;
@@ -619,13 +624,133 @@ static void hdmitx_set_timming(int mode)
         G385_HDMITX_REG->g385_reserved[18] = 0x00000000;
     }
     else if (mode == 1) { //HDMITX 720x576 50Hz as default setting
-        ; //TBD
+        G380_HDMITX_REG->g380_reserved[5] = 0x0000141f;
+        G380_HDMITX_REG->g380_reserved[6] = 0x000000ff;
+        G380_HDMITX_REG->g380_reserved[8] = 0x00001001;
+
+        G381_HDMITX_REG->g381_reserved[16] = 0x00000140;
+		G381_HDMITX_REG->g381_reserved[20] = 0x00000716;
+
+        G382_HDMITX_REG->g382_reserved[12] = 0x00000043;
+        G382_HDMITX_REG->g382_reserved[16] = 0x0000e411;
+        G382_HDMITX_REG->g382_reserved[17] = 0x000001b5;
+        G382_HDMITX_REG->g382_reserved[18] = 0x0000007e;
+        G382_HDMITX_REG->g382_reserved[26] = 0x00000085;
+        G382_HDMITX_REG->g382_reserved[27] = 0x00001800;
+        G382_HDMITX_REG->g382_reserved[28] = 0x00000000;
+
+        G383_HDMITX_REG->g383_reserved[1] = 0x00000003;
+        G383_HDMITX_REG->g383_reserved[2] = 0x00000004;
+        G383_HDMITX_REG->g383_reserved[3] = 0x00000000;
+        G383_HDMITX_REG->g383_reserved[7] = 0x00005018;
+        G383_HDMITX_REG->g383_reserved[8] = 0x000000a0;
+        G383_HDMITX_REG->g383_reserved[11] = 0x00000010;
+
+        G385_HDMITX_REG->g385_reserved[0] = 0x00001a1a;
+        G385_HDMITX_REG->g385_reserved[1] = 0x0000101a;
+        G385_HDMITX_REG->g385_reserved[2] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[3] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[4] = 0x00000010;
+        G385_HDMITX_REG->g385_reserved[5] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[6] = 0x00001cf9;
+        G385_HDMITX_REG->g385_reserved[7] = 0x00000058;
+        G385_HDMITX_REG->g385_reserved[8] = 0x00000011;
+        G385_HDMITX_REG->g385_reserved[9] = 0x00000024;
+        G385_HDMITX_REG->g385_reserved[10] = 0x00000204;
+        G385_HDMITX_REG->g385_reserved[11] = 0x0000007a;
+        G385_HDMITX_REG->g385_reserved[12] = 0x0000034a;
+        G385_HDMITX_REG->g385_reserved[13] = 0x00000170;
+        G385_HDMITX_REG->g385_reserved[14] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[15] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[16] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[17] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[18] = 0x00000000;
     }
     else if (mode == 2) { //HDMITX 1280x720 59.94Hz as default setting
-		; //TBD
+        G380_HDMITX_REG->g380_reserved[5] = 0x0000141f;
+        G380_HDMITX_REG->g380_reserved[6] = 0x000000ff;
+        G380_HDMITX_REG->g380_reserved[8] = 0x00001001;
+
+        G381_HDMITX_REG->g381_reserved[16] = 0x00000140;
+		G381_HDMITX_REG->g381_reserved[20] = 0x00000110;
+
+        G382_HDMITX_REG->g382_reserved[12] = 0x00000043;
+        G382_HDMITX_REG->g382_reserved[16] = 0x0000e411;
+        G382_HDMITX_REG->g382_reserved[17] = 0x000001b5;
+        G382_HDMITX_REG->g382_reserved[18] = 0x0000007e;
+        G382_HDMITX_REG->g382_reserved[26] = 0x00000085;
+        G382_HDMITX_REG->g382_reserved[27] = 0x00001800;
+        G382_HDMITX_REG->g382_reserved[28] = 0x00000000;
+
+        G383_HDMITX_REG->g383_reserved[1] = 0x00000003;
+        G383_HDMITX_REG->g383_reserved[2] = 0x00000004;
+        G383_HDMITX_REG->g383_reserved[3] = 0x00000000;
+        G383_HDMITX_REG->g383_reserved[7] = 0x00005018;
+        G383_HDMITX_REG->g383_reserved[8] = 0x000000a0;
+        G383_HDMITX_REG->g383_reserved[11] = 0x00000010;
+
+        G385_HDMITX_REG->g385_reserved[0] = 0x00001a1a;
+        G385_HDMITX_REG->g385_reserved[1] = 0x0000101a;
+        G385_HDMITX_REG->g385_reserved[2] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[3] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[4] = 0x00000010;
+        G385_HDMITX_REG->g385_reserved[5] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[6] = 0x00001cb6;
+        G385_HDMITX_REG->g385_reserved[7] = 0x000000a8;
+        G385_HDMITX_REG->g385_reserved[8] = 0x00000004;
+        G385_HDMITX_REG->g385_reserved[9] = 0x00000024;
+        G385_HDMITX_REG->g385_reserved[10] = 0x00000204;
+        G385_HDMITX_REG->g385_reserved[11] = 0x0000007a;
+        G385_HDMITX_REG->g385_reserved[12] = 0x0000034a;
+        G385_HDMITX_REG->g385_reserved[13] = 0x00000170;
+        G385_HDMITX_REG->g385_reserved[14] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[15] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[16] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[17] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[18] = 0x00000000;
 	}
 	else if (mode == 3) { //HDMITX 1920x1080 59.94Hz as default setting
-		; //TBD
+        G380_HDMITX_REG->g380_reserved[5] = 0x0000141f;
+        G380_HDMITX_REG->g380_reserved[6] = 0x000000ff;
+        G380_HDMITX_REG->g380_reserved[8] = 0x00001001;
+
+        G381_HDMITX_REG->g381_reserved[16] = 0x00000140;
+		G381_HDMITX_REG->g381_reserved[20] = 0x00000210;
+
+        G382_HDMITX_REG->g382_reserved[12] = 0x00000043;
+        G382_HDMITX_REG->g382_reserved[16] = 0x0000e411;
+        G382_HDMITX_REG->g382_reserved[17] = 0x000001b5;
+        G382_HDMITX_REG->g382_reserved[18] = 0x0000007e;
+        G382_HDMITX_REG->g382_reserved[26] = 0x00000085;
+        G382_HDMITX_REG->g382_reserved[27] = 0x00001800;
+        G382_HDMITX_REG->g382_reserved[28] = 0x00000000;
+
+        G383_HDMITX_REG->g383_reserved[1] = 0x00000003;
+        G383_HDMITX_REG->g383_reserved[2] = 0x00000004;
+        G383_HDMITX_REG->g383_reserved[3] = 0x00000000;
+        G383_HDMITX_REG->g383_reserved[7] = 0x00005018;
+        G383_HDMITX_REG->g383_reserved[8] = 0x000000a0;
+        G383_HDMITX_REG->g383_reserved[11] = 0x00000010;
+
+        G385_HDMITX_REG->g385_reserved[0] = 0x00001a1a;
+        G385_HDMITX_REG->g385_reserved[1] = 0x0000101a;
+        G385_HDMITX_REG->g385_reserved[2] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[3] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[4] = 0x00000010;
+        G385_HDMITX_REG->g385_reserved[5] = 0x00001010;
+        G385_HDMITX_REG->g385_reserved[6] = 0x00001caa;
+        G385_HDMITX_REG->g385_reserved[7] = 0x000000a8;
+        G385_HDMITX_REG->g385_reserved[8] = 0x00000010;
+        G385_HDMITX_REG->g385_reserved[9] = 0x00000024;
+        G385_HDMITX_REG->g385_reserved[10] = 0x00000204;
+        G385_HDMITX_REG->g385_reserved[11] = 0x0000007a;
+        G385_HDMITX_REG->g385_reserved[12] = 0x0000034a;
+        G385_HDMITX_REG->g385_reserved[13] = 0x00000170;
+        G385_HDMITX_REG->g385_reserved[14] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[15] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[16] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[17] = 0x00000000;
+        G385_HDMITX_REG->g385_reserved[18] = 0x00000000;
 	}
 
 }
