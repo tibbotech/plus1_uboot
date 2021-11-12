bba8bd754341c69e63ae12e2d7ec78258984a4f7
Dvorkin Dmitry <dvorkin@tibbo.com>
2021-11-12 15:43:27 +0300

    [configs] nand read dtb does not really need to calc dtb size

--- include/configs/pentagram_common.h
+++ include/configs/pentagram_common.h
@@ -289,12 +289,13 @@
 	"setexpr sz_dtb ${tmpval} + 0x28; " \
 	"setexpr sz_dtb ${sz_dtb} + 0x200; setexpr sz_dtb ${sz_dtb} / 0x200; " \
 	"mmc read ${addr_dst_dtb} ${addr_src_dtb} ${sz_dtb}; "
-#define DTS_LOAD_NAND \
-	"nand read ${addr_tmp_header} dtb 0x40; " \
-	"setenv tmpval 0; setexpr tmpaddr ${addr_tmp_header} + 0x4; run be2le; " \
-	"setexpr sz_dtb ${tmpval} + 0x28; " \
-	"setexpr sz_dtb ${sz_dtb} + 0x200; setexpr sz_dtb ${sz_dtb} / 0x200; " \
-	"nand read ${addr_dst_dtb} dtb ${sz_dtb}; "
+//#define DTS_LOAD_NAND \
+//	"nand read ${addr_tmp_header} dtb 0x40; " \
+//	"setenv tmpval 0; setexpr tmpaddr ${addr_tmp_header} + 0x4; run be2le; " \
+//	"setexpr sz_dtb ${tmpval} + 0x28; " \
+//	"setexpr sz_dtb ${sz_dtb} + 0x200; setexpr sz_dtb ${sz_dtb} / 0x200; " \
+//	"nand read ${addr_dst_dtb} dtb ${sz_dtb}; "
+#define DTS_LOAD_NAND "nand read ${addr_dst_dtb} dtb; "
 #else
 #define DTS_LOAD_ADDR "${fdtcontroladdr}"
 #define DTS_LOAD_EMMC
