95604e1a4dcb21f20923c4d6cde6c1ce34566e89
Wells Lu <wells.lu@sunplus.com>
2022-03-30 16:36:58 +0800

    [drivers][pinctrl][sunplus] Added GPIO driver.

--- drivers/pinctrl/sunplus/pinctrl_sunplus.c
+++ drivers/pinctrl/sunplus/pinctrl_sunplus.c
@@ -1,7 +1,10 @@
 #include <common.h>
 #include <dm.h>
+#include <dm/device-internal.h>
+#include <dm/lists.h>
 #include <dm/pinctrl.h>
 #include <asm/io.h>
+#include <asm-generic/gpio.h>
 
 #include "pinctrl_sunplus.h"
 #ifdef CONFIG_PINCTRL_SUNPLUS
@@ -37,15 +40,23 @@ void pinmux_grps_dump(void)
 	func_t *func = &list_funcs[i];
 	for (i = 0; i < list_funcsSZ; i++) {
 		func = &(list_funcs[i]);
-		if (func->gnum == 0) continue;
-		if (func->freg != fOFF_G) continue;
+
+		if (func->gnum == 0)
+			continue;
+
+		if (func->freg != fOFF_G)
+			continue;
+
 		mask = (1 << func->blen) - 1;
 		rval = GPIO_PINGRP(func->roff);
 		val = (rval >> func->boff) & mask;
-		if (val == 0) continue;
+		if (val == 0)
+			continue;
+
 		printf("%s\t=%d regval:%X\n", list_funcs[i].name, val, rval);
 	}
 }
+
 #ifdef SUPPORT_PINMUX
 void pinmux_reg_dump(void)
 {
@@ -53,9 +64,8 @@ void pinmux_reg_dump(void)
 	int i;
 
 	printf("pinmux_reg_dump \n");
-	for (i = MUXF_L2SW_CLK_OUT; i <= MUXF_GPIO_INT7; i++) {
-		pinmux_value[i-MUXF_L2SW_CLK_OUT]  = gpio_pin_mux_get(i);
-	}
+	for (i = MUXF_L2SW_CLK_OUT; i <= MUXF_GPIO_INT7; i++)
+		sp_gpio_pin_mux_get(i, &pinmux_value[i-MUXF_L2SW_CLK_OUT]);
 
 	printf("l2sw = 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x \n",
 		pinmux_value[0], pinmux_value[1], pinmux_value[2], pinmux_value[3], pinmux_value[4],
@@ -105,12 +115,12 @@ void pinmux_reg_dump(void)
 
 void gpio_reg_dump(void)
 {
-	int i;
 	u32 gpio_value[MAX_PINS];
+	int i;
 
 	printf("gpio_reg_dump (FI MA IN II OU OI OE OD)\n");
 	for (i = 0; i < MAX_PINS; i++) {
-		gpio_value[i] = gpio_para_get(i);
+		gpio_value[i] = sp_gpio_para_get(i);
 		if ((i%8) == 7) {
 			printf("GPIO_P%-2d 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x\n",(i/8),
 			gpio_value[i-7],gpio_value[i-6],gpio_value[i-5],gpio_value[i-4],
@@ -128,7 +138,7 @@ void gpio_reg_dump(void)
 }
 #endif
 
-static void* register_pin(int pin, struct udevice *dev)
+static void* sp_register_pin(int pin, struct udevice *dev)
 {
 	// Check if pin number is within range.
 	if ((pin >= 0) && (pin < MAX_PINS)) {
@@ -137,10 +147,11 @@ static void* register_pin(int pin, struct udevice *dev)
 			// save a pointer to the registering device.
 			pin_registered_by_udev[pin] = dev;
 			pctl_info("Register pin %d (from node: %s).\n", pin, dev->name);
+
 			return 0;
 		} else {
 			pctl_err("ERROR: Pin %d of node %s has been registered (by node: %s)!\n",
-				pin, dev->name, ((struct udevice*)pin_registered_by_udev[pin])->name);
+				 pin, dev->name, ((struct udevice*)pin_registered_by_udev[pin])->name);
 			return dev;
 		}
 	} else {
@@ -149,23 +160,26 @@ static void* register_pin(int pin, struct udevice *dev)
 	}
 }
 
-static int unregister_pin(int pin)
+static int sp_unregister_pin(int pin)
 {
 	// Check if pin number is within range.
-	if ((pin >= 0) && (pin < MAX_PINS)) {
+	if ((pin >= 0) && (pin < MAX_PINS))
 		if (pin_registered_by_udev[pin]) {
 			pin_registered_by_udev[pin] = 0;
 			return 0;
-		 }
-	}
+		}
+
 	return -1;
 }
 
-static int sunplus_pinctrl_pins(struct udevice *dev)
+static int sp_pinctrl_pins(struct udevice *dev)
 {
 	int offset = dev_of_offset(dev);
 	u32 pin_mux[MAX_PINS];
 	int len, i;
+#ifdef SUPPORT_PINMUX
+	u32 val;
+#endif
 
 	// Get property: "sunplus,pins"
 	len = fdtdec_get_int_array_count(gd->fdt_blob, offset, "sunplus,pins",
@@ -192,11 +206,13 @@ found_pins:
 	for (i = 0; i < len; i++) {
 		int pin  = (pin_mux[i] >> 24) & 0xff;
 
-		if (register_pin(pin, dev) != 0) break;
+		if (sp_register_pin(pin, dev) != 0)
+			break;
 	}
 
 	// If any pin was not registered successfully, return -1.
-	if ((len > 0) && (i != len)) return -1;
+	if ((len > 0) && (i != len))
+		return -1;
 
 	// All pins were registered successfully, set up all pins.
 	for (i = 0; i < len; i++) {
@@ -212,54 +228,57 @@ found_pins:
 #ifdef SUPPORT_PINMUX
 		if (type == SPPCTL_PCTL_G_PMUX) {
 			// It's a PinMux pin.
-			gpio_pin_mux_set(func, pin);
-			gpio_first_set(pin, 0);
-			gpio_master_set(pin, 1);
-			pctl_info("pinmux get = 0x%02x \n", gpio_pin_mux_get(func));
+			sp_gpio_pin_mux_set(func, pin);
+			sp_gpio_first_set(pin, 0);
+			sp_gpio_master_set(pin, 1);
+			sp_gpio_pin_mux_get(func, &val);
+			pctl_info("pinmux get = 0x%02x \n", val);
 		} else
 #endif
 		if (type == SPPCTL_PCTL_G_IOPP) {
 			// It's a IOP pin.
-			gpio_first_set(pin, 1);
-			gpio_master_set(pin, 0);
+			sp_gpio_first_set(pin, 1);
+			sp_gpio_master_set(pin, 0);
 		} else if (type == SPPCTL_PCTL_G_GPIO) {
 			// It's a GPIO pin.
-			gpio_first_set(pin, 1);
-			gpio_master_set(pin, 1);
+			sp_gpio_first_set(pin, 1);
+			sp_gpio_master_set(pin, 1);
 
 			if (flag & (SPPCTL_PCTL_L_OUT|SPPCTL_PCTL_L_OU1)) {
-				if (flag & SPPCTL_PCTL_L_OUT) {
-					gpio_out_set(pin, 0);
-				}
-				if (flag & SPPCTL_PCTL_L_OU1) {
-					gpio_out_set(pin, 1);
-				}
-				gpio_oe_set(pin, 1);
+				if (flag & SPPCTL_PCTL_L_OUT)
+					sp_gpio_out_set(pin, 0);
+
+				if (flag & SPPCTL_PCTL_L_OU1)
+					sp_gpio_out_set(pin, 1);
+
+				sp_gpio_oe_set(pin, 1);
 			} else if (flag & SPPCTL_PCTL_L_ODR) {
-				gpio_open_drain_set(pin, 1);
+				sp_gpio_open_drain_set(pin, 1);
 			}
 
-			if (flag & SPPCTL_PCTL_L_INV) {
-				gpio_input_invert_set(pin, 1);
-			} else {
-				gpio_input_invert_set(pin, 0);
-			}
-			if (flag & SPPCTL_PCTL_L_ONV) {
-				gpio_output_invert_set(pin, 1);
-			} else {
-				gpio_output_invert_set(pin, 0);
-			}
+			if (flag & SPPCTL_PCTL_L_INV)
+				sp_gpio_input_invert_set(pin, 1);
+			else
+				sp_gpio_input_invert_set(pin, 0);
+
+			if (flag & SPPCTL_PCTL_L_ONV)
+				sp_gpio_output_invert_set(pin, 1);
+			else
+				sp_gpio_output_invert_set(pin, 0);
 		}
 	}
 
 	return 0;
 }
 
-static int sunplus_pinctrl_zero(struct udevice *dev)
+static int sp_pinctrl_zero(struct udevice *dev)
 {
 	int offset = dev_of_offset(dev);
 	u32 pin_mux[MAX_PINS];
 	int len, i, mask;
+#ifdef SUPPORT_PINMUX
+	u32 val;
+#endif
 
 	// Get property: "sunplus,zerofunc"
 	len = fdtdec_get_int_array_count(gd->fdt_blob, offset, "sunplus,zerofunc",
@@ -288,37 +307,40 @@ found_zero_func:
 		pctl_info("zero_func = 0x%08x\n", func);
 
 		// Set it to no use.
-		if ( func >= list_funcsSZ) {
+		if (func >= list_funcsSZ) {
 			pctl_info("func=%d is out of range, skipping...\n", func);
 			continue;
 		}
-		func_t *f = &list_funcs[ func];
-		switch ( f->freg) {
+
+		func_t *f = &list_funcs[func];
+		switch (f->freg) {
 #ifdef SUPPORT_PINMUX
-			case fOFF_M:
-				gpio_pin_mux_set( func, 0);
-				pctl_info( "pinmux get = 0x%02x\n", gpio_pin_mux_get( func));
-				break;
+		case fOFF_M:
+			sp_gpio_pin_mux_set(func, 0);
+			sp_gpio_pin_mux_get(func, &val);
+			pctl_info("pinmux get = 0x%02x\n", val);
+			break;
 #endif
-			case fOFF_G:
-				mask = ( 1 << f->blen) - 1;
-				GPIO_PINGRP( f->roff) = ( mask << ( f->boff+16)) | ( 0 << f->boff);
-				pctl_info( "group %s set to 0\n", f->name);
-				break;
-			default:
-				printf( "bad zero func/group idx:%d, skipped\n", func);
-				break;
+		case fOFF_G:
+			mask = (1 << f->blen) - 1;
+			GPIO_PINGRP(f->roff) = (mask << (f->boff+16)) | (0 << f->boff);
+			pctl_info("group %s set to 0\n", f->name);
+			break;
+
+		default:
+			printf("bad zero func/group idx:%d, skipped\n", func);
+			break;
 		}
 	}
 
 	return 0;
 }
 
-static int sunplus_pinctrl_function(struct udevice *dev)
+static int sp_pinctrl_function(struct udevice *dev)
 {
 	int offset = dev_of_offset(dev);
-	const char *pin_func;
 	const char *pin_group;
+	const char *pin_func;
 	int len, i;
 
 	// Get property: 'sppctl,function'
@@ -336,13 +358,18 @@ found_function:
 	if (len > 1) {
 		// Find 'function' string in list: only groups
 		for (i = 0; i < list_funcsSZ; i++) {
-			if ( list_funcs[ i].gnum == 0) continue;
-			if ( list_funcs[ i].freg != fOFF_G) continue;
-			if ( strcmp( pin_func, list_funcs[i].name) == 0) break;
+			if (list_funcs[i].gnum == 0)
+				continue;
+
+			if (list_funcs[i].freg != fOFF_G)
+				continue;
+
+			if (strcmp(pin_func, list_funcs[i].name) == 0)
+				break;
 		}
 		if (i == list_funcsSZ) {
 			pctl_err("Error: Invalid 'function' in node %s! "
-				"Cannot find \"%s\"!\n", dev->name, pin_func);
+				 "Cannot find \"%s\"!\n", dev->name, pin_func);
 			return -1;
 		}
 
@@ -361,12 +388,13 @@ found_groups:
 			func_t *func = &list_funcs[i];
 
 			// Find 'pin_group' string in list.
-			for (i = 0; i < func->gnum; i++) {
-				if (strcmp (pin_group, func->grps[i].name) == 0) break;
-			}
+			for (i = 0; i < func->gnum; i++)
+				if (strcmp (pin_group, func->grps[i].name) == 0)
+					break;
+
 			if (i == func->gnum) {
 				pctl_err("Error: Invalid 'groups' in node %s! "
-					"Cannot find \"%s\"!\n", dev->name, pin_group);
+					 "Cannot find \"%s\"!\n", dev->name, pin_group);
 				return -1;
 			}
 
@@ -374,9 +402,9 @@ found_groups:
 			const sppctlgrp_t *grp = &func->grps[i];
 
 			// Register all pins of the group.
-			for (i = 0; i < grp->pnum; i++) {
-				if (register_pin(grp->pins[i], dev) != 0) break;
-			}
+			for (i = 0; i < grp->pnum; i++)
+				if (sp_register_pin(grp->pins[i], dev) != 0)
+					break;
 
 			// Check if all pins of the group register successfully
 			if (i == grp->pnum) {
@@ -403,18 +431,20 @@ found_groups:
 	return 0;
 }
 
-static int sunplus_pinctrl_set_state(struct udevice *dev, struct udevice *config)
+static int sp_pinctrl_set_state(struct udevice *dev, struct udevice *config)
 {
 	int ret;
 
-	pctl_info("\nConfig node: %s\n",config->name);
-	ret = sunplus_pinctrl_pins(config);
-	if (ret != 0) return ret;
+	pctl_info("\nConfig node: %s\n", config->name);
+	ret = sp_pinctrl_pins(config);
+	if (ret != 0)
+		return ret;
 
-	sunplus_pinctrl_zero(config);
+	sp_pinctrl_zero(config);
 
-	ret = sunplus_pinctrl_function(config);
-	if (ret != 0) return ret;
+	ret = sp_pinctrl_function(config);
+	if (ret != 0)
+		return ret;
 
 #ifdef PINCTRL_DEBUG
 #ifdef SUPPORT_PINMUX
@@ -427,8 +457,120 @@ static int sunplus_pinctrl_set_state(struct udevice *dev, struct udevice *config
 	return 0;
 }
 
+static struct pinctrl_ops sunplus_pinctrl_ops = {
+	.set_state = sp_pinctrl_set_state,
+};
+
+static int sp_gpio_request(struct udevice *dev, unsigned int offset, const char *label)
+{
+	int err;
+
+	pctl_info("%s: offset = %u, label = %s\n", __func__, offset, label);
+
+	err = sp_gpio_first_set(offset, 1);
+	if (err)
+		return err;
+
+	err = sp_gpio_master_set(offset, 1);
+	if (err)
+		return err;
+
+	return 0;
+}
+
+static int sp_gpio_rfree(struct udevice *dev, unsigned int offset)
+{
+	pctl_info("%s: offset = %u\n", __func__, offset);
+
+	//sp_gpio_first_set(offset, 0);
+	//sp_gpio_master_set(offset, 0);
+
+	return 0;
+}
+
+static int sp_gpio_get_value(struct udevice *dev, unsigned int offset)
+{
+	u32 val;
+	int err;
+
+	pctl_info("%s: offset = %u\n", __func__, offset);
+
+	err = sp_gpio_in(offset, &val);
+	if (err)
+		return err;
+
+	return !!val;
+}
+
+static int sp_gpio_set_value(struct udevice *dev, unsigned int offset, int val)
+{
+	pctl_info("%s: offset = %u, val = %d\n", __func__, offset, val);
+
+	return sp_gpio_out_set(offset, !!val);
+}
+
+static int sp_gpio_get_function(struct udevice *dev, unsigned int offset)
+{
+	u32 val;
+	int err;
+
+	pctl_info("%s: offset = %u\n", __func__, offset);
+
+	err = sp_gpio_oe_get(offset, &val);
+	if (err)
+		return err;
+
+	return val ? GPIOF_OUTPUT : GPIOF_INPUT;
+}
+
+static int sp_gpio_direction_input(struct udevice *dev, unsigned int offset)
+{
+	pctl_info("%s: offset = %u\n", __func__, offset);
+
+	return sp_gpio_oe_set(offset, 0);
+}
+
+static int sp_gpio_direction_output(struct udevice *dev, unsigned int offset, int val)
+{
+	pctl_info("%s: offset = %u, val = %d\n", __func__, offset, val);
+
+	sp_gpio_out_set(offset, val);
+
+	return sp_gpio_oe_set(offset, 1);
+}
+
+static int sp_gpio_probe(struct udevice *dev)
+{
+	struct gpio_dev_priv *uc_priv;
+
+	uc_priv = dev_get_uclass_priv(dev);
+	uc_priv->bank_name = "GPIO";
+	uc_priv->gpio_count = MAX_PINS;
+
+	return 0;
+}
+
+static const struct dm_gpio_ops sp_gpio_ops = {
+	.request = sp_gpio_request,
+	.rfree = sp_gpio_rfree,
+	.set_value = sp_gpio_set_value,
+	.get_value = sp_gpio_get_value,
+	.get_function = sp_gpio_get_function,
+	.direction_input = sp_gpio_direction_input,
+	.direction_output = sp_gpio_direction_output,
+};
+
+static struct driver sp_gpio_driver = {
+	.name  = "sunplus_gpio",
+	.id    = UCLASS_GPIO,
+	.probe = sp_gpio_probe,
+	.ops   = &sp_gpio_ops,
+};
+
 static int sunplus_pinctrl_probe(struct udevice *dev)
 {
+	struct udevice *cdev;
+	ofnode node;
 	int i;
 
 #ifdef SUPPORT_PINMUX
@@ -453,9 +595,8 @@ static int sunplus_pinctrl_probe(struct udevice *dev)
 	// Get address of GPIOXT2 registers.
 	gpioxt2_regs = (void*)devfdt_get_addr_name(dev, "gpioxt2");
 	pctl_info("gpioxt2_regs = %px\n", gpioxt2_regs);
-	if (gpioxt2_regs == (void*)-1) {
+	if (gpioxt2_regs == (void*)-1)
 		gpioxt2_regs = gpioxt_regs + 0x80;
-	}
 #endif
 
 	// Get address of FIRST registers.
@@ -475,35 +616,42 @@ static int sunplus_pinctrl_probe(struct udevice *dev)
 	}
 
 	// Unregister all pins.
-	for (i = 0; i < MAX_PINS; i++) {
-		unregister_pin(i);
+	for (i = 0; i < MAX_PINS; i++)
+		sp_unregister_pin(i);
+
+	// Bind gpio driver.
+	node = dev_ofnode(dev);
+	if (ofnode_read_bool(node, "gpio-controller")) {
+		if (!lists_uclass_lookup(UCLASS_GPIO)) {
+			pctl_err("Cannot find GPIO driver\n");
+			return -ENOENT;
+		}
+
+		return device_bind(dev, &sp_gpio_driver, "sunplus_gpio",
+				   0, node, &cdev);
 	}
 
 	return 0;
 }
 
-static struct pinctrl_ops sunplus_pinctrl_ops = {
-	.set_state      = sunplus_pinctrl_set_state,
-};
-
 static const struct udevice_id sunplus_pinctrl_ids[] = {
 #ifdef CONFIG_PINCTRL_SUNPLUS
-	{ .compatible = "sunplus,sp7021-pctl"},
+	{ .compatible = "sunplus,sp7021-pctl" },
 #elif defined (CONFIG_PINCTRL_SUNPLUS_Q645)
-	{ .compatible = "sunplus,q645-pctl"},
+	{ .compatible = "sunplus,q645-pctl" },
 #elif defined (CONFIG_PINCTRL_SUNPLUS_SP7350)
-	{ .compatible = "sunplus,sp7350-pctl"},
+	{ .compatible = "sunplus,sp7350-pctl" },
 #else
-	{ .compatible = "sunplus,i143-pctl"},
+	{ .compatible = "sunplus,i143-pctl" },
 #endif
 	{ /* zero */ }
 };
 
 U_BOOT_DRIVER(pinctrl_sunplus) = {
-	.name           = "sunplus_pinctrl",
-	.id             = UCLASS_PINCTRL,
-	.probe          = sunplus_pinctrl_probe,
-	.of_match       = sunplus_pinctrl_ids,
-	.ops            = &sunplus_pinctrl_ops,
-	.bind           = dm_scan_fdt_dev,
+	.name     = "sunplus_pinctrl",
+	.id       = UCLASS_PINCTRL,
+	.probe    = sunplus_pinctrl_probe,
+	.of_match = sunplus_pinctrl_ids,
+	.ops      = &sunplus_pinctrl_ops,
+	.bind     = dm_scan_fdt_dev,
 };
--- drivers/pinctrl/sunplus/pinctrl_sunplus.h
+++ drivers/pinctrl/sunplus/pinctrl_sunplus.h
@@ -93,7 +93,7 @@ typedef struct {
 
 typedef struct {
 	const char * const name;
-	const fOFF_t freg;          // function register type
+	const fOFF_t freg;              // function register type
 	const u8 roff;                  // register offset
 	const u8 boff;                  // bit offset
 	const u8 blen;                  // number of bits
@@ -114,33 +114,23 @@ extern volatile u32 *gpioxt2_regs;
 #endif
 extern volatile u32 *first_regs;
 
-extern int gpio_pin_mux_set(u32 func, u32 pin);
-extern int gpio_pin_mux_get_val(u32 func, u32 *pin);
-extern u32 gpio_pin_mux_get(u32 func);
-extern int gpio_input_invert_set(u32 bit, u32 val);
-extern int gpio_input_invert_get(u32 bit, u32 *val);
-extern u32 gpio_input_invert_val_get(u32 bit);
-extern int gpio_output_invert_set(u32 bit, u32 val);
-extern int gpio_output_invert_get(u32 bit, u32 *val);
-extern u32 gpio_output_invert_val_get(u32 bit);
-extern int gpio_open_drain_set(u32 bit, u32 val);
-extern int gpio_open_drain_get(u32 bit, u32 *val);
-extern u32 gpio_open_drain_val_get(u32 bit);
-extern int gpio_first_set(u32 bit, u32 val);
-extern int gpio_first_get(u32 bit, u32 *val);
-extern u32 gpio_first_val_get(u32 bit);
-extern int gpio_master_set(u32 bit, u32 val);
-extern int gpio_master_get(u32 bit, u32 *val);
-extern u32 gpio_master_val_get(u32 bit);
-extern int gpio_oe_set(u32 bit, u32 val);
-extern int gpio_oe_get(u32 bit, u32 *val);
-extern u32 gpio_oe_val_get(u32 bit);
-extern int gpio_out_set(u32 bit, u32 val);
-extern int gpio_out_get(u32 bit, u32 *val);
-extern u32 gpio_out_val_get(u32 bit);
-extern int gpio_in(u32 pin, u32 *val);
-extern u32 gpio_in_val(u32 bit);
-extern u32 gpio_para_get(u32 pin);
-extern int gpio_debug_set(u32 bit, u32 val);
+int sp_gpio_pin_mux_set(u32 func, u32 pin);
+int sp_gpio_pin_mux_get(u32 func, u32 *pin);
+int sp_gpio_input_invert_set(u32 offset, u32 value);
+int sp_gpio_input_invert_get(u32 offset, u32 *value);
+int sp_gpio_output_invert_set(u32 offset, u32 value);
+int sp_gpio_output_invert_get(u32 offset, u32 *value);
+int sp_gpio_open_drain_set(u32 offset, u32 value);
+int sp_gpio_open_drain_get(u32 offset, u32 *value);
+int sp_gpio_first_set(u32 offset, u32 value);
+int sp_gpio_first_get(u32 offset, u32 *value);
+int sp_gpio_master_set(u32 offset, u32 value);
+int sp_gpio_master_get(u32 offset, u32 *value);
+int sp_gpio_oe_set(u32 offset, u32 value);
+int sp_gpio_oe_get(u32 offset, u32 *value);
+int sp_gpio_out_set(u32 offset, u32 value);
+int sp_gpio_out_get(u32 offset, u32 *value);
+int sp_gpio_in(u32 offset, u32 *value);
+u32 sp_gpio_para_get(u32 offset);
 
 #endif
