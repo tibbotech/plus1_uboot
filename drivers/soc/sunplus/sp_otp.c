602b88428b68bc192ce14096a802f782ef428681
Vincent Shih <vincent.shih@sunplus.com>
2022-05-11 19:39:40 +0800

    [Q645][drivers][otp] Fix a bug for the write.

--- drivers/soc/sunplus/sp_otp.c
+++ drivers/soc/sunplus/sp_otp.c
@@ -254,14 +254,20 @@ static int do_write_otp(struct cmd_tbl  *cmdtp, int flag, int argc, char * const
 	regs->sft_cfg[0] = 0x003c0008;
 
 	if (efuse == 0) {
-		if (write_otp_data(HB_GP_REG, SP_OTPRX_REG, addr, &value) == -1)
+		if (write_otp_data(HB_GP_REG, SP_OTPRX_REG, addr, &value) == -1) {
+			regs->sft_cfg[0] = 0xffff0000 | cfg;
 			return CMD_RET_FAILURE;
+		}
 	} else if (efuse == 1) {
-		if (write_otp_data(KEY_HB_GP_REG, KEY_OTPRX_REG, addr, &value) == -1)
+		if (write_otp_data(KEY_HB_GP_REG, KEY_OTPRX_REG, addr, &value) == -1) {
+			regs->sft_cfg[0] = 0xffff0000 | cfg;
 			return CMD_RET_FAILURE;
+		}
 	} else if (efuse == 2) {
-		if (write_otp_data(CUSTOMER_HB_GP_REG, CUSTOMER_OTPRX_REG, addr, &value) == -1)
+		if (write_otp_data(CUSTOMER_HB_GP_REG, CUSTOMER_OTPRX_REG, addr, &value) == -1) {
+			regs->sft_cfg[0] = 0xffff0000 | cfg;
 			return CMD_RET_FAILURE;
+		}
 	}
 
 	regs->sft_cfg[0] = 0xffff0000 | cfg;
