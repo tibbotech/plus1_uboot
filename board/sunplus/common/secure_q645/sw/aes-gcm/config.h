68e5521b4691112d0502415105380e13731dc451
haiq.tang <haiq.tang@sunmedia.com.cn>
2022-12-15 20:02:16 +0800

    [Q645][secure] delete unused files

--- board/sunplus/common/secure_q645/sw/aes-gcm/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define MBEDTLS_GCM_C
-#define MBEDTLS_CIPHER_C
-#define MBEDTLS_AES_C
-#define MBEDTLS_SELF_TEST
--- board/sunplus/common/secure_q645/sw/libeddsa/apps/put.sh
+++
@@ -1,4 +0,0 @@
-cp -v sign_ed_shaX ../../../../xboot/tools/
-cp -v mkey_ed_shaX ../../../../xboot/tools/
-cp -v mkey_x25519 ../../../../xboot/tools/
-cp -v x25519_ss ../../../../xboot/tools/
