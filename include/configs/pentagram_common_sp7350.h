47c78790744f076cd41ccc91f06df9ea2a807cce
haiq.tang <haiq.tang@sunmedia.com.cn>
2023-03-28 11:41:46 +0800

    [Q645][Q654][OPTEE] set uboot run address from 0x300000 to 0x500000. Adjust the ENV partition info of emmc/nand for add fip partition

--- include/configs/pentagram_common_sp7350.h
+++ include/configs/pentagram_common_sp7350.h
@@ -99,9 +99,9 @@
 //#define CONFIG_MTD_DEVICE		/* needed for mtdparts cmd */
 #define MTDIDS_DEFAULT			"nand0=sp_spinand.0"
 #if 0 // Set default mtdparts for zebu sim
-#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),512k(env),512k(env_redund),1m(reserve),256k(dtb),25m(kernel),33536k(rootfs)"
+#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),1m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),33536k(rootfs)"
 #else
-#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),512k(env),512k(env_redund),1m(reserve),256k(dtb),25m(kernel),230144k(rootfs)"
+#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),1m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),230144k(rootfs)"
 #endif
 #ifdef CONFIG_SP_PARANAND
 #undef CONFIG_SYS_NAND_BASE
