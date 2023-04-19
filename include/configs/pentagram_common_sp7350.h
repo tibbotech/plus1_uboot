303edc36c509cf3fd5363d74853fb1cb8827e17b
Wells Lu <wells.lu@sunplus.com>
2023-04-19 10:29:17 +0800

    [SP7350] Fixed wrong size of rootfs partition of default mtdparts for Zebu sim.

--- include/configs/pentagram_common_sp7350.h
+++ include/configs/pentagram_common_sp7350.h
@@ -99,7 +99,7 @@
 //#define CONFIG_MTD_DEVICE		/* needed for mtdparts cmd */
 #define MTDIDS_DEFAULT			"nand0=sp_spinand.0"
 #if 0 // Set default mtdparts for zebu sim
-#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),32536k(rootfs)"
+#define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),32512k(rootfs)"
 #else
 #define MTDPARTS_DEFAULT		"sp_spinand.0:128k(nand_header),384k(xboot1),1536k(uboot1),2048k(uboot2),2m(fip),512k(env),512k(env_redund),256k(dtb),25m(kernel),229120k(rootfs)"
 #endif
